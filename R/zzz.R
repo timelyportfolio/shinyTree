52996823084827279a07361820c2d7edb6f9bfb1
timelyportfolio <kent.russell@timelyportfolio.com>
2023-07-15 11:08:48 -0500

    handle `NULL` with input handler; increment minor version

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,8 @@
     shiny::registerInputHandler(
       "shinyTree.changed",
       function(x, session, inputName) {
+        if(is.null(x$value)) return(NULL)
+        
         result <- x$value
         attr(result, "type") <- x$type
         attr(result, "action") <- x$action
